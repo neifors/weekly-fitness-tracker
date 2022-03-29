d0edad71d434d64ebd9901fa408d2a0310ef460b
Paul Dobrescu <paul.dobrescu10@gmail.com>
2022-03-29 16:15:12 +0100

    Functions ready

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -43,13 +43,13 @@ function renderHomepage() {
 
 
 function renderLoginForm() {
-   const signIn = document.createElement('h2')
+   const signIn = document.createElement('h3')
    signIn.id = 'signin-title'
    signIn.textContent = 'Sign in Below'
    main.appendChild(signIn)
 
    const fields = [
-       { tag: 'input', attributes: { type: 'email', name: 'email', placeholder: 'E.g. fitnessperson@gmail.com' } },
+       { tag: 'input', attributes: { type: 'email', name: 'email', placeholder: 'Enter your email here' } },
        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Please insert password here' } },
        { tag: 'input', attributes: { type: 'submit', value: 'Login' } }
    ]
@@ -62,5 +62,44 @@ function renderLoginForm() {
          form.appendChild(field);
       })
    })
-   
+   form.addEventListener('submit', requestLogin)
+   main.appendChild(form);
+
+   const question = document.createElement('h4');
+   question.id = 'are-you-new';
+   question.textContent = "Don't have an account yet? Sign up!";
+   main.appendChild(question);
+
+   const registerBttn = document.createElement('button');
+   registerBttn.id = 'register-button';
+   registerBttn.textContent = 'Sign Up Now';
+   registerBttn.onclick = registerRedirect;
+   main.appendChild(registerBttn);
+}
+
+function renderRegisterForm() {
+   const fields = [
+      { tag: 'input', attributes: { type: 'text', name: 'username', placeholder: 'Create a username' } },
+      { tag: 'input', attributes: { type: 'email', name: 'email', placeholder: 'Your email E.g. fitnessperson@gmail.com' } },
+      { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Create a password' } },
+      { tag: 'input', attributes: { type: 'password', name: 'passwordConfirmation', placeholder: 'Confirm Password' } },
+      { tag: 'input', attributes: { type: 'submit', value: 'Create Account' } }
+   ]
+
+   const form = document.createElement('form');
+   fields.forEach(f => {
+      let field = document.createElement(f.tag);
+      Object.entries(f.attributes).forEach(([a, v]) => {
+         field.setAttribute(a, v);
+         form.appendChild(field);
+      })
+   })
+   form.addEventListener('submit', requestRegistration)
+   main.appendChild(form);
+}
+
+function render404() {
+   const error = document.createElement('h3');
+   error.textContent = "Oops, we can't find that page sorry!";
+   main.appendChild(error);
 }
