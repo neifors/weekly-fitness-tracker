4f3f6fe0bb394743e5caa8a274af15b1fdd56316
Leanne Sobogun <leannesobogun@gmail.com>
2022-04-01 12:58:07 +0100

    deploying

--- server/db_config/dbconfig.js
+++ server/db_config/dbconfig.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb')
-const connectionUrl = process.env.DB_CONNECTION||'mongodb://futureproof:userpass@db:27017'
+const connectionUrl = process.env.MONGODB_URI||'mongodb://futureproof:userpass@db:27017'
 
 const dbName = process.env.DB_NAME||'users'
 
