920ae2e7e1fdda09c48bb2c93c27ab6deea934df
isabel <isargp@gmail.com>
2022-09-07 11:26:08 +0100

    cors config modified again in server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const cors= require('cors');
 const server=express();
-server.use(cors("*"));
+server.use(cors({
+    origin: "https://weekly-fitness-tracker.netlify.app",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+}));
 server.use(express.json());
 
 
